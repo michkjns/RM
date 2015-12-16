52f0e92231d5ba25b812f8e8f1504886c55a8508
shrekleton <michkjn@gmail.com>
2015-12-16 14:48:12 +0100

    Updated BitStream class
    - Fixed a bug in BitStream::WriteBits
    - Added size specific read/write functions for integers

--- RocketMen/src/bitstream.cpp
+++ RocketMen/src/bitstream.cpp
@@ -12,8 +12,8 @@ public:
 	BitStream_impl();
 	~BitStream_impl();
 
-	void writeBits(bool value, size_t numBits /* = 1 */) override;
-	void writeBytes(char value, size_t numBytes /* = 1 */) override;
+	void writeBits(bool value, size_t repeat /* = 1 */) override;
+	void writeBytes(char value, size_t repeat /* = 1 */) override;
 	void writeFloat(float value) override;
 	void writeInt(int value) override;
 	void writeBool(bool value) override;
@@ -21,10 +21,12 @@ public:
 	void writeBuffer(char* data, size_t length) override;
 	void writeData(char* data, size_t length) override;
 
-	void ReadBytes(char* output, size_t numBytes /* = 1 */) override;
+	void ReadBytes(char* output, size_t size /* = 1 */) override;
 	void readBit(bool* output) override;
 	float readFloat() override;
-	int readInt() override;
+	int8_t readInt8() override;
+	int32_t readInt32() override;
+	int64_t readInt64() override;
 	bool readBool() override;
 
 
@@ -113,10 +115,24 @@ float BitStream_impl::readFloat()
 	return out;
 }
 
-int BitStream_impl::readInt()
+int8_t BitStream_impl::readInt8()
 {
 	int out;
-	ReadBytes(reinterpret_cast<char*>(&out), sizeof(int));
+	ReadBytes(reinterpret_cast<char*>(&out), sizeof(int8_t));
+	return out;
+}
+
+int32_t BitStream_impl::readInt32()
+{
+	int out;
+	ReadBytes(reinterpret_cast<char*>(&out), sizeof(int32_t));
+	return out;
+}
+
+int64_t BitStream_impl::readInt64()
+{
+	int out;
+	ReadBytes(reinterpret_cast<char*>(&out), sizeof(int64_t));
 	return out;
 }
 
@@ -137,9 +153,9 @@ void BitStream_impl::writeBuffer(char* data, size_t length)
 	m_writeTotalBytes = static_cast<int>(length);
 }
 
-void BitStream_impl::writeBytes(char value, size_t numBytes)
+void BitStream_impl::writeBytes(char value, size_t repeat)
 {
-	for (int writeByte = 0; writeByte < (int)numBytes; writeByte++)
+	for (int writeByte = 0; writeByte < (int)repeat; writeByte++)
 	{
 		if (m_writeBit == 0)
 		{
@@ -161,46 +177,36 @@ void BitStream_impl::writeBytes(char value, size_t numBytes)
 	}
 }
 
-void BitStream_impl::writeBits(bool value, size_t numBits)
+void BitStream_impl::writeBits(bool value, size_t repeat /* = 1 */)
 {
 	//Don't overflow the buffer
 	assert(m_writeTotalBytes < BUFFERLENGTH);
 	if (m_writeTotalBytes >= BUFFERLENGTH) return /*false*/;
 
-	int numBytes = (int)floorf((float)numBits / 8);
+	int32_t numBytes = (int32_t)floorf((float)repeat / 8);
 	if (numBytes >= 1)
 	{
 		writeBytes(value, numBytes);
-		numBits -= 8 * numBytes;
+		repeat -= 8 * numBytes;
 	}
 
 	std::bitset<8> bits(*m_writeData);
-	
-	int stopAt = m_writeBit + static_cast<int>(numBits);
-	for (m_writeBit; m_writeBit < stopAt; m_writeBit++)
+
+	for (unsigned int i = 0; i < repeat; i++)
 	{
 		if (m_writeBit == 8)
 		{
+			*m_writeData = static_cast<char>(bits.to_ulong());
 			m_writeData++;
 			bits = std::bitset<8>(*m_writeData);
 			m_writeBit = 0;
 			m_writeTotalBytes++;
-			stopAt -= 8;
 		}
-		bits[m_writeBit] = value;
-	
+
+		bits[m_writeBit++] = value;
 	}
 
-	//if (m_writeBit == 8)
-	//{
-	//	m_writeData++;
-	//	m_writeBit = 0;
-	//	m_writeTotalBytes++;
-
-	//}
-
 	*m_writeData = static_cast<char>(bits.to_ulong());
-
 }
 
 void BitStream_impl::writeData(char* data, size_t length)
--- RocketMen/src/bitstream.h
+++ RocketMen/src/bitstream.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include <stdint.h>
+
 class BitStream
 {
 public:
@@ -11,8 +13,8 @@ public:
 	// Write data appending to the current buffer
 	virtual void writeData(char* data, size_t length) = 0;
 
-	virtual void writeBits(bool value, size_t numBits = 1) = 0;
-	virtual void writeBytes(char value, size_t numBytes = 1) = 0;
+	virtual void writeBits(bool value, size_t repeat = 1) = 0;
+	virtual void writeBytes(char value, size_t repeat = 1) = 0;
 	virtual void writeFloat(float value) = 0;
 	virtual void writeInt(int value) = 0;
 	virtual void writeBool(bool value) = 0;
@@ -21,7 +23,9 @@ public:
 	virtual void ReadBytes(char* output, size_t numBytes = 1) = 0;
 	virtual void readBit(bool* output) = 0;
 	virtual float readFloat() = 0;
-	virtual int readInt() = 0;
+	virtual int8_t readInt8() = 0;
+	virtual int32_t readInt32() = 0;
+	virtual int64_t readInt64() = 0;
 	virtual bool readBool() = 0;
 
 	virtual const size_t getLength() const = 0;
